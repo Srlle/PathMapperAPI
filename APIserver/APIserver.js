ae02263b797fbf0593ab753b5506cd78d6911c07
Srlle <srdan.kovacevic@gmail.com>
2017-10-14 22:52:59 +0200

    MVP of the API server and an example of a client app

--- APIserver/APIserver.js
+++ APIserver/APIserver.js
@@ -4,20 +4,45 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Path} = require('./models/path');
+
 const app = express();
 
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-// DATA STRUCTURE
-// USER_Schema
-// TRACK_Schema:
-// User_field: user_id [string]
-// Points_field: [(lon,lat), (lon,lat)]
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-// MobApp sends POST request to /point route (lon, lat)
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get('/testpath', (req,res) => {
+  var points = [
+    {lat: 37.772, lng: -122.214},
+    {lat: 21.291, lng: -157.821},
+    {lat: -18.142, lng: 178.431},
+    {lat: -27.467, lng: 153.027}
+  ];
+  return res.send(points).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
 
 app.post('/newpath', (req,res) => {
   var path = new Path({
@@ -36,9 +61,9 @@ app.post('/point', (req, res) => {
 
   var point = req.body.point;
 
-  Path.findOneAndUpdate(req.body.path_name, {$push: {points: point}}).then((path_name) => {
+  Path.findOneAndUpdate({"path_name": req.body.path_name}, {$push: {points: point}}).then((path_name) => {
     if (!path_name) {
-      return res.status(404).send(`Run with name ${req.body.path_name} not found.`);
+      return res.status(404).send(`Path with name ${req.body.path_name} not found.`);
     }
     res.send(point);
   }).catch((e) => {
@@ -51,7 +76,7 @@ app.get('/path/:path_name', (req,res) => {
   var path_name = req.params.path_name;
 
   Path.findOne({path_name: path_name}).then((path) => {
-    return res.send(path.points)
+    return res.send(JSON.stringify(path.points))
   }).catch((e) => {
     res.status(400).send();
   })
